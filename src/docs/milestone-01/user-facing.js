2cf0d5103ea2494a38afce01f48718689139123b
Tapugy <jakeitan@gmail.com>
2024-04-03 23:03:29 -0400

    Updates files system

--- src/docs/milestone-01/user-facing.js
+++ src/docs/milestone-01/user-facing.js
@@ -2,7 +2,7 @@ const doc = document.getElementById('temp-doc')
 const counter = document.getElementById('word-count')
 
 
-doc.value= 'Welcome to our first prototype. Feel free to type, you can view the word count at the bottom of the page. We will add more features later '
+doc.value= 'Welcome to our first prototype. Feel free to type, you can view the word count at the bottom of the page. We will add more features later. '
 
 function wordcount()
 {
