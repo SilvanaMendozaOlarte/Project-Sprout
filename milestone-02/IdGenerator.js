45c4665461c236012ac0c8c32a558bf0968b5d8e
Donald Winkelman <dmwink.tx@gmail.com>
2024-04-23 18:48:57 -0400

    Commenting and working on shop navbar

--- milestone-02/IdGenerator.js
+++ milestone-02/IdGenerator.js
@@ -3,15 +3,20 @@ class IdGenerator {
     constructor() {
         if (this.IdGenerator === null) {
             this.IdGenerator = this
-            this.project_id = 0
-            this.task_id = 0
-            this.pomodoro_id = 0
+            this.project_id = -1
+            this.task_id = -1
+            this.pomodoro_id = -1
         }
         return ChangePage.ChangePage
     }
-
     generateProjectId() {
         return this.project_id++
+    }
+    generateTaskId() {
+        return this.task_id++
+    }
+    generatePomodoroId() {
+        return this.pomodoro_id++
     }
 }
 
