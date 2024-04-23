164adfdd68624391796348dd6ba45d74962bce20
SilvanaMendozaOlarte <silvanamendozaolarte@gmail.com>
2024-04-23 19:05:11 -0400

    th and td to div

--- milestone-02/Calendar.js
+++ milestone-02/Calendar.js
@@ -2,15 +2,16 @@ const buildCalendar = () => {
     // > placeholder, should be dynamic
     numRows = 8;
     days_of_week = [
-        "Mon",
-        "Tue",
-        "Wed",
-        "Thu",
-        "Fri",
-        "Sat",
-        "Sun"
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat",
+    "Sun"
     ];
     calendar = document.getElementById("calendar");
+  
     for (let i = 0; i < 7; i++){
         for (let j = 0; j < numRows; j++){
 
@@ -20,21 +21,22 @@ const buildCalendar = () => {
 
             // > creates a th if on first row, else creates a td
             if(j > 1){
-                day_item = document.createElement("th");
+                day_item = document.createElement("div");
                 text = document.createTextNode(days_of_week[i]);
             }
             else{
-                day_item = document.createElement("td");
+                day_item = document.createElement("div");
                 // > placeholder, will display day number, will do l8r
                 text = document.createTextNode(0);
             }
 
             day_item.classList.add("day_item");
             calendar.appendChild(day_item);  
-            // !adding click function to every day
-            // square.addEventListener("click", onclick(i,j));
+    
+            //! adding click function to every day
+            //square.addEventListener("click", onclick(i,j));
+
         }
     }
 }
-
 buildCalendar();
