0d32f880c15d7b06d6e7cfa9225431de5cdfc465
Donald Winkelman <dmwink.tx@gmail.com>
2024-04-24 00:06:12 -0400

    Images downloaded and worked on ChangeShopCategory

--- milestone-02/ChangeShopCategory.js
+++ milestone-02/ChangeShopCategory.js
@@ -2,15 +2,33 @@ class ChangeShopCategory {
     ChangeShopCategory = null
     constructor() {
         if (this.ChangeShopCategory === null) {
-            this.prevCategory = "sign_in"
+            this.prevCategory = null
             ChangeShopCategory.ChangeShopCategory = this
         }
         return ChangeShopCategory.ChangeShopCategory
     }
-    ChangeShopCategory(category) {
+    changeShopCategory(category, data) {
         document.getElementById(this.prevCategory).style.display = 'none'
         document.getElementById(category).style.display = 'block'
         this.prevCategory = category
+        if (category === 'shop_category_choose_avatar') {
+            this.#renderAvatar(data)
+        }
+        if (category === 'shop_category_purchased_items') {
+            this.#renderPurchasedCategories(data)
+        }
+        if (category === 'shop_category_house' || category === 'shop_category_garden' || category === 'shop_category_plants' || category === 'shop_category_avatar') {
+            this.#renderBuyCategories(data, category)
+        }
+    }
+    #renderBuyCategories(data) {
+        
+    }
+    #renderPurchasedCategories(data) {
+
+    }
+    #renderAvatar(data) {
+
     }
 }
 
