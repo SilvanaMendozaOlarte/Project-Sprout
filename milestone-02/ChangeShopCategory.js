9c3278ba5138e62794795db165458c2e98141af6
SilvanaMendozaOlarte <silvanamendozaolarte@gmail.com>
2024-04-25 15:00:27 -0400

    Revert "Merge branch 'Donald' into main-02"
    This reverts commit a84ddf569490539087814508520bac29b8571d15, reversing
    changes made to 0cc611fe5d2d1cc013fdbcb005033c9a7d3f1763.

--- milestone-02/ChangeShopCategory.js
+++ milestone-02/ChangeShopCategory.js
@@ -2,33 +2,15 @@ class ChangeShopCategory {
     ChangeShopCategory = null
     constructor() {
         if (this.ChangeShopCategory === null) {
-            this.prevCategory = null
+            this.prevCategory = "sign_in"
             ChangeShopCategory.ChangeShopCategory = this
         }
         return ChangeShopCategory.ChangeShopCategory
     }
-    changeShopCategory(category, data) {
+    ChangeShopCategory(category) {
         document.getElementById(this.prevCategory).style.display = 'none'
         document.getElementById(category).style.display = 'block'
         this.prevCategory = category
-        if (category === 'shop_category_choose_avatar') {
-            this.#renderAvatar(data)
-        }
-        if (category === 'shop_category_purchased_items') {
-            this.#renderPurchasedCategories(data)
-        }
-        if (category === 'shop_category_house' || category === 'shop_category_garden' || category === 'shop_category_plants' || category === 'shop_category_avatar') {
-            this.#renderBuyCategories(data, category)
-        }
-    }
-    #renderBuyCategories(data) {
-        
-    }
-    #renderPurchasedCategories(data) {
-
-    }
-    #renderAvatar(data) {
-
     }
 }
 
